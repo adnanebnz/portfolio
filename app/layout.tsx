c03f3c1dace5d4c616e7d39774acb3233823ac69
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-10-04 20:05:29 +0100

    added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:title" content="BENZERDJEB Adnane" />
+        <meta property="og:description" content="Software Engineer" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dxobgdfyq/image/upload/v1696446212/nsz6yiupgwfv6syv9diy.png"
+        />
+        <meta
+          property="og:url"
+          content="https://skillzdev-portfolio.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
       </head>
       <body className="font-bodyFont w-full h-screen bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
         <Navbar />

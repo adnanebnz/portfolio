c46c57a18ba03f5d5eebff134792acec87c2be41
skillz <skillzdev@hotmail.com>
2023-10-15 18:15:14 +0100

    modified CV

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -80,7 +80,7 @@ function Navbar() {
           </ul>
           <a
             href={
-              "https://drive.google.com/file/d/1SiEXgfwDmOpjpJsKkju6jjLNQBlJTxh4/view?usp=sharing"
+              "https://drive.google.com/file/d/1KRtm4D77kURRaWtnpTYpvg3HzdwbTx7R/view?usp=sharing"
             }
             target="_blank"
           >
@@ -164,7 +164,7 @@ function Navbar() {
                   </li>
                 </ul>
                 <a
-                  href="https://drive.google.com/file/d/1SiEXgfwDmOpjpJsKkju6jjLNQBlJTxh4/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1KRtm4D77kURRaWtnpTYpvg3HzdwbTx7R/view?usp=sharing"
                   target="_blank"
                 >
                   <button className="px-5 py-3 rounded-md text-textBlue text-[17px] border border-textBlue hover:bg-hoverColor duration-300 mt-10">
